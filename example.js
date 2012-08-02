3be689f4e72278478c4bbc4d6258a6cb67c4a156
devyn <devyn.cairns@gmail.com>
2012-08-02 03:57:56 -0700

    Convert to spaces

--- example.js
+++ example.js
@@ -1,115 +1,115 @@
 var timer
   , trace_queue    = []
-	, identify_queue = []
-	;
+  , identify_queue = []
+  ;
 
 function log(message) {
-	postMessage({type: "log", message: message});
+  postMessage({type: "log", message: message});
 }
 
 function identify() {
-	identify_queue.push(function (identity) {
-		if (identity) {
-			log("I've identified the "+identity.type+", "+identity.name);
-			evade(search);
-		} else {
-			log("I don't know what that is. Continuing search.");
-			timer = setTimeout(search, 250);
-		}
-	});
+  identify_queue.push(function (identity) {
+    if (identity) {
+      log("I've identified the "+identity.type+", "+identity.name);
+      evade(search);
+    } else {
+      log("I don't know what that is. Continuing search.");
+      timer = setTimeout(search, 250);
+    }
+  });
 
-	postMessage({type: "identify"});
+  postMessage({type: "identify"});
 }
 
 function evade(then) {
-	postMessage({type: "update", vx: 0, vy: -60, va: Math.PI/6});
+  postMessage({type: "update", vx: 0, vy: -60, va: Math.PI/6});
 
-	setTimeout(then, 1000);
+  setTimeout(then, 1000);
 }
 
 var sc = 0;
 
 function changeSearchRoute() {
-	postMessage({type: "update", vx: 40, vy: 0, va: Math.PI*(Math.random()-0.5)});
+  postMessage({type: "update", vx: 40, vy: 0, va: Math.PI*(Math.random()-0.5)});
 }
 
 function continueSearch() {
-	trace_queue.push(function (distance) {
-		if (distance) {
-			log("I saw something " + distance.toFixed(2) + " pixels away. Checking it out.");
-			postMessage({type: "update", vx: 0, vy: 0, va: 0});
-			identify();
-		} else {
-			if (++sc >= 3) { sc = 0; changeSearchRoute(); }
+  trace_queue.push(function (distance) {
+    if (distance) {
+      log("I saw something " + distance.toFixed(2) + " pixels away. Checking it out.");
+      postMessage({type: "update", vx: 0, vy: 0, va: 0});
+      identify();
+    } else {
+      if (++sc >= 3) { sc = 0; changeSearchRoute(); }
 
-			timer = setTimeout(continueSearch, 250);
-		}
-	});
+      timer = setTimeout(continueSearch, 250);
+    }
+  });
 
-	postMessage({type: "trace"});
+  postMessage({type: "trace"});
 }
 
 function search() {
-	sc = 0;
-	changeSearchRoute();
-	continueSearch();
+  sc = 0;
+  changeSearchRoute();
+  continueSearch();
 }
 
 function stop() {
-	clearTimeout(timer);
-	timer = null;
-	trace_queue    = [];
-	identify_queue = [];
+  clearTimeout(timer);
+  timer = null;
+  trace_queue    = [];
+  identify_queue = [];
 }
 
 function identifiedBy(angle, direction) {
-	stop();
+  stop();
 
-	var sign = 1;
-	if (angle < 0) sign = -1;
+  var sign = 1;
+  if (angle < 0) sign = -1;
 
-	postMessage({type: "update", vx: 0, vy: 0, va: Math.PI * sign});
+  postMessage({type: "update", vx: 0, vy: 0, va: Math.PI * sign});
 
-	timer = setTimeout(function () {
-		identify();
-	}, Math.abs(angle/Math.PI)*1000);
+  timer = setTimeout(function () {
+    identify();
+  }, Math.abs(angle/Math.PI)*1000);
 }
 
 function collision() {
-	stop();
+  stop();
 
-	postMessage({type: "update", vx: 0, vy: 40, va: Math.PI/8});
+  postMessage({type: "update", vx: 0, vy: 40, va: Math.PI/8});
 
-	timer = setTimeout(search, 1000);
+  timer = setTimeout(search, 1000);
 }
 
 onmessage = function (event) {
-	var message = event.data;
+  var message = event.data;
 
-	switch (message.type) {
-		case "trace_hit":
-			if (trace_queue.length > 0)
-				trace_queue.shift()(message.distance);
-			break;
-		case "trace_miss":
-			if (trace_queue.length > 0)
-				trace_queue.shift()(null);
-			break;
-		case "identified":
-			if (identify_queue.length > 0)
-				identify_queue.shift()(message.identity);
-			break;
-		case "no_identity":
-			if (identify_queue.length > 0)
-				identify_queue.shift()(null);
-			break;
-		case "identified_by":
-			identifiedBy(message.angle, message.direction);
-			break;
-		case "collision":
-			collision();
-			break;
-	}
+  switch (message.type) {
+    case "trace_hit":
+      if (trace_queue.length > 0)
+        trace_queue.shift()(message.distance);
+      break;
+    case "trace_miss":
+      if (trace_queue.length > 0)
+        trace_queue.shift()(null);
+      break;
+    case "identified":
+      if (identify_queue.length > 0)
+        identify_queue.shift()(message.identity);
+      break;
+    case "no_identity":
+      if (identify_queue.length > 0)
+        identify_queue.shift()(null);
+      break;
+    case "identified_by":
+      identifiedBy(message.angle, message.direction);
+      break;
+    case "collision":
+      collision();
+      break;
+  }
 };
 
 search();
