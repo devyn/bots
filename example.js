cd991c97029d01c1969e5f2ca2e2da27082d2eef
devyn <devyn.cairns@gmail.com>
2012-08-02 04:25:30 -0700

    Maybe more subtle.

--- example.js
+++ example.js
@@ -83,10 +83,10 @@ function identifiedBy(angle, distance) {
 function collision() {
   stop();
 
-  postMessage({type: "update", vx: 0, vy: 40, va: Math.PI});
+  postMessage({type: "update", vx: 0, vy: 40, va: Math.PI/2});
 
   timer = setTimeout(function () {
-    postMessage({type: "update", va: -Math.PI});
+    postMessage({type: "update", va: -Math.PI/2});
 
     timer = setTimeout(search, 500);
   }, 500);
