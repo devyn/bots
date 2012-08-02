98038831817e67d6fc8bc13021c266945c5be926
devyn <devyn.cairns@gmail.com>
2012-08-02 03:44:16 -0700

    hmm

--- bots.js
+++ bots.js
@@ -53,8 +53,8 @@ Bot.prototype.processMessage = function (message) {
       break;
     case "identify":
       console.log(this.name + " attempted identify");
-      if (this.target/* && typeof this.target.identify === "function"*/) {
-        this.script.postMessage({type: "identified", identity: this.target.identify(this)});
+      if (this.target && typeof this.target.object.identify === "function") {
+        this.script.postMessage({type: "identified", identity: this.target.object.identify(this)});
       } else {
         this.script.postMessage({type: "no_identity"});
       }
