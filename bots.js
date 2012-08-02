e687019ada25a2ed30a4d3d9bb29999af6be6bd4
devyn <devyn.cairns@gmail.com>
2012-08-02 04:14:11 -0700

    normalize the damn angle

--- bots.js
+++ bots.js
@@ -66,7 +66,7 @@ Bot.prototype.processMessage = function (message) {
 Bot.prototype.identify = function (from) {
   var d_x      = from.x - this.x
     , d_y      = from.y - this.y
-    , origin_a = from.a - Math.atan2(d_y, d_x) - Math.PI/2
+    , origin_a = Math.atan2(Math.sin(from.a), Math.cos(from.a)) - Math.atan2(d_y, d_x) - Math.PI/2
     , origin_d = Math.sqrt(d_x*d_x + d_y*d_y)
     ;
 
